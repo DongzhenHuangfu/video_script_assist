cce7ce6942f97f67d0162567cef132c94f277ad9
DongzhenHuangfu <wyatt_h@outlook.com>
2025-02-27 09:52:07 +0800

    feat: 更改文字

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import traceback
 from utils import generate_script
 
-st.title("🎬 视频脚本生成器")
+st.title("🎬 短视频脚本生成工具")
 
 with st.sidebar:
     api_key = st.text_input("Deepseek API密钥：", type="password")
@@ -11,15 +11,15 @@ with st.sidebar:
 
 subject = st.text_input("💡 请输入视频的主题")
 video_length = st.number_input("⏱️ 请输入视频的大致时长（单位：分钟）", min_value=0.1, step=0.1)
-creativity = st.slider("✨ 请输入视频脚本的创造力（数字小说明更严谨，数字大说明更多样）", min_value=0.0,
+creativity = st.slider("✨ 请输入视频脚本的创造力（数字小说明更严谨，数字大说明更具有创造性）", min_value=0.0,
                        max_value=1.0, value=0.2, step=0.1)
 submit = st.button("生成脚本")
 
 if submit and not api_key:
-    st.info("请输入你的Deepseek API密钥")
+    st.info("Deepseek API密钥未输入")
     st.stop()
 if submit and not subject:
-    st.info("请输入视频的主题")
+    st.info("视频的主题未输入")
     st.stop()
 if submit and not video_length >= 0.1:
     st.info("视频长度需要大于或等于0.1")
